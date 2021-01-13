9a85c618f3992cd7eace87112bcf1a61aefa830c
Nicolas Bauwens <nicolas.bauwens@gmail.com>
2021-01-13 19:14:17 +0100

    First commit: index and detail page

---
+++ pages/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              Camps zéro déchet
+            </h1>
+            <h2 class="subtitle">
+              Fiches recettes
+            </h2>
+          </div>
+        </div>
+      </section>
+
+      <div class="container mt-6" v-show="loading">
+        ça arrive...
+      </div>
+
+      <div class="container">
+        <div class="columns mt-6">
+            <div class="column is-one-quarter" v-for="recette in recettes" :key="recette.slug">
+              <div class="card" >
+                <div class="card-content">
+                  <p class="title">
+                    {{recette.nom}}
+                  </p>
+                  <span class="tag is-success is-light">{{recette.diet}}</span>
+                </div>
+                <footer class="card-footer">
+                  <p class="card-footer-item">
+                    <span>
+                      <nuxt-link :to="{path: '/recette/'+recette.slug}">Voir la recette</nuxt-link>
+                    </span>
+                  </p>
+                  <p class="card-footer-item">
+                    <span>
+                      Ajouter au menu
+                    </span>
+                  </p>
+                </footer>
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+     data () {
+        return {
+            recettes: [],
+            loading: true
+        }
+    },
+    mounted(){
+        return axios({
+            url: "https://api.baseql.com/airtable/graphql/apptVpg9XpET0IEyv",
+            method: "post",
+            data: {
+            query: `{
+                recettes(_order_by: "nom") {
+                  nom
+                  slug,
+                  diet
+                }
+            }`,
+            },
+        }).then((result) => {
+            console.log(result.data)
+            this.recettes = result.data.data.recettes;
+            this.loading = false;
+        })
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ pages/recette/_index.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+      <section class="hero">
+        <div class="hero-body">
+          <div class="container">
+            <nuxt-link to="/">&lt; Retour</nuxt-link>
+            <h1 class="title pt-2">
+              {{recette.nom}}
+            </h1>
+          </div>
+        </div>
+      </section>
+
+        <div class="container">  
+            <div class="columns">
+                <div class="column is-half">
+                     <div class="field is-horizontal">
+                        <div class="field-label">
+                            <label class="label">Nombre de couverts</label>
+                        </div>
+                        <div class="field-body">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input" type="text" v-model="plates" style="width: 11%;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            <div class="columns">
+                <div class="column">
+                    <h2 class="is-size-4">Ingrédients</h2>
+                    <div v-for="compo in recette.compositions" :key="compo.nom" class="columns m-0 p-0">
+                        <div class="column">
+                            {{Math.floor(compo.quantite * plateRatio)}} {{compo.unites}} {{compo.nomIngredient[0]}}
+                        </div>
+                        <div class="column">
+                            {{ (compo.prix * plateRatio).toFixed(2) }} €
+                        </div>
+                    </div>
+                    <div class="columns">
+                        <div class="column"><strong>Prix du plat</strong></div>
+                        <div class="column">{{(recette.prix * plateRatio).toFixed(2)}} €</div>
+                    </div>
+                </div>
+                <div class="column">
+                    <h2 class="is-size-4">Préparation</h2>
+                    <div v-html="procedure"></div>
+                </div>
+            </div>            
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+import marked from 'marked'
+
+export default {
+     data () {
+        return {
+            recette: null,
+            plates: 0
+        }
+    },
+    computed: {
+        procedure(){
+            return marked(this.recette.procedure)
+        },
+        plateRatio(){
+            return this.plates / this.recette.nombreDePersonnes;
+        }
+    },
+    asyncData({ params, error, payload }){
+        console.log(payload);
+        console.log(params)
+        if (payload){
+            return { recette: payload };
+        }
+        else {
+            console.log("get...")
+            return axios({
+                url: "https://api.baseql.com/airtable/graphql/apptVpg9XpET0IEyv",
+                method: "post",
+                data: {
+                query: `{
+                   recettes(slug: "${params.index}") {
+                        nom
+                        nombreDePersonnes
+                        procedure
+                        compositions {
+                            nom
+                            quantite
+                            unites
+                            nomIngredient
+                            prix
+                        }
+                        prix
+                    }
+                }`,
+                },
+            }).then((result) => {
+                console.log(result.data)
+                return {
+                    recette: result.data.data.recettes[0],
+                    plates: result.data.data.recettes[0].nombreDePersonnes
+                }
+            })
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
